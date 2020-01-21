cbda0a4433f5209d84b7a14c492b52e4dfb6184f
buyaobishige <1105936025@163.com>
2020-01-21 10:50:45 +0800

    121

--- assets/scripts/addCards.js
+++ assets/scripts/addCards.js
@@ -2,15 +2,131 @@ cc.Class({
   extends: cc.Component,
 
   properties: {
+    dataJSON: {
+      default: null,
+      type: cc.JsonAsset
+    },
+    necessaryItems: "",
     cardItem: {
       default: null,
       type: cc.Prefab
+    },
+    foodbtn: {
+      default: null,
+      type: cc.Node
+    },
+    stationerybtn: {
+      default: null,
+      type: cc.Node
+    },
+    bookbtn: {
+      default: null,
+      type: cc.Node
     }
   },
 
   // LIFE-CYCLE CALLBACKS:
+  init() {
+    let json = this.dataJSON.json;
+    if (!window.theJSONOfAllItems) {
+      cc.log(cc.sys.localStorage.getItem("itemList"));
+      if (cc.sys.localStorage.getItem("itemList")) {
+        window.theJSONOfAllItems = JSON.parse(
+          cc.sys.localStorage.getItem("itemList")
+        );
+      } else {
+        window.theJSONOfAllItems = json;
+        cc.sys.localStorage.setItem("itemList", JSON.stringify(json));
+      }
+    }
+    let necessaryItemsArray = this.necessaryItems.split(" ");
+    let thingsInListArray = { food: [], stationery: [], book: [] };
+    json.map(item => {
+      if (
+        necessaryItemsArray.includes(item.itemName) &&
+        item.category == "食物"
+      ) {
+        thingsInListArray.food.push(item);
+      } else if (
+        necessaryItemsArray.includes(item.itemName) &&
+        item.category == "文具"
+      ) {
+        thingsInListArray.stationery.push(item);
+      } else if (
+        necessaryItemsArray.includes(item.itemName) &&
+        item.category == "典籍"
+      ) {
+        thingsInListArray.book.push(item);
+      }
+    });
+    window.thingsInListArray = thingsInListArray;
 
+    if (!window.thingsInListCategory) {
+      window.thingsInListCategory = "食物";
+    }
+
+    let N;
+    if (window.thingsInListCategory == "食物") {
+      N = window.thingsInListArray.food.length;
+    } else if (window.thingsInListCategory == "文具") {
+      N = window.thingsInListArray.stationery.length;
+    } else if (window.thingsInListCategory == "典籍") {
+      N = window.thingsInListArray.book.length;
+      // cc.log(JSON.stringify(window.thingsInListArray));
+      // cc.log(N);
+    }
+    this.loadCards(N);
+  },
   onLoad() {
+    //金钱重置为1000
+    if (!window.theMoneyOfPlayer) {
+      if (
+        cc.sys.localStorage.getItem("theMoneyOfPlayer") != null &&
+        (cc.sys.localStorage.getItem("theMoneyOfPlayer") != null) != undefined
+      ) {
+        window.theMoneyOfPlayer = cc.sys.localStorage.getItem(
+          "theMoneyOfPlayer"
+        );
+        // cc.log(cc.sys.localStorage);
+      } else {
+        window.theMoneyOfPlayer = 90000;
+        cc.sys.localStorage.setItem("theMoneyOfPlayer", 90000);
+      }
+    }
+
+    //------------
+
+    this.init();
+    this.foodbtn.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        cc.log("foodbtn");
+        window.thingsInListCategory = "食物";
+        this.init();
+      },
+      this
+    );
+    this.stationerybtn.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        cc.log("stationerybtn");
+        window.thingsInListCategory = "文具";
+        this.init();
+      },
+      this
+    );
+    this.bookbtn.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        cc.log("bookbtn");
+        window.thingsInListCategory = "典籍";
+        this.init();
+      },
+      this
+    );
+  },
+  loadCards(n) {
+    this.node.removeAllChildren(true);
     //被参考的第一个card的相对位置
     let x0 = 737;
     let x1 = 230;
@@ -19,21 +135,18 @@ cc.Class({
     let offsety = 318;
     //=======
     let i = 0;
-    let n = 10;
-    // console.log(this.node);
     this.node.height = n % 2 == 0 ? 0.5 * n * 350 : (n + 1) * 0.5 * 350;
 
     let nodeArray = [];
+    // cc.log(n);
     while (i < n) {
       let node = cc.instantiate(this.cardItem);
       nodeArray.push(node);
       i++;
     }
-    console.log(nodeArray);
+    // cc.log(i);
     let j = 0;
-    // console.log(nodeArray[j]);
     while (j < n) {
-      // console.log(nodeArray[j]);
       nodeArray[j].x = x1;
       nodeArray[j].y = y0;
       nodeArray[j].parent = this.node;
@@ -48,10 +161,6 @@ cc.Class({
       y0copy -= offsety;
       w += 2;
     }
-    console.log(this.node._children);
-  },
-
-  start() {}
-
+  }
   // update (dt) {},
 });
--- assets/scripts/itemCard.js
+++ assets/scripts/itemCard.js
@@ -1,37 +1,79 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
   extends: cc.Component,
 
   properties: {
-    intro: { default: "这是一件商品。", type: cc.String },
-    price: { default: 0, type: cc.Float },
-    itemName: { default: "", type: cc.String },
-    img: { default: null, type: cc.SpriteFrame },
-    introBox: { default: null, type: cc.Label },
-    priceBox: { default: null, type: cc.Label },
-    imgBox: { default: null, type: cc.Node },
-    nameBox: { default: null, type: cc.Label }
+    itemName: { default: null, type: cc.Label },
+    intro: { default: null, type: cc.Label },
+    price: { default: null, type: cc.Label },
+    img: { default: null, type: cc.Node },
+    buybtn: { default: null, type: cc.Node },
+    buybtnsensor: { default: null, type: cc.Node },
+    hasOwned: { default: null, type: cc.Label }
   },
 
   // LIFE-CYCLE CALLBACKS:
 
   onLoad() {
-    // this.introBox.string = "        " + this.intro;
-    // this.priceBox.string = this.price;
-    // this.nameBox.string = this.itemName;
-    // this.introBox.SpriteFrame = this.img;
+    // cc.log("1" + JSON.stringify(window.thingsInListArray));
+    let item;
+    if (window.thingsInListCategory == "食物") {
+      item = window.thingsInListArray.food.pop();
+    } else if (window.thingsInListCategory == "文具") {
+      item = window.thingsInListArray.stationery.pop();
+    } else if (window.thingsInListCategory == "典籍") {
+      item = window.thingsInListArray.book.pop();
+    }
+    // cc.log("2" + JSON.stringify(window.thingsInListArray));
+    this.itemName.string = item.itemName;
+    this.intro.string = "        " + item.intro;
+    this.price.string = String(item.price);
+    window.theJSONOfAllItems.map(obj => {
+      if (obj.itemName == item.itemName) {
+        this.hasOwned.string = "已拥有：" + String(obj.quantity);
+      }
+    });
+
+    let imgnode = this.img;
+    cc.loader.loadRes(item.img, cc.SpriteFrame, function(err, spriteFrame) {
+      var node = new cc.Node("New Sprite");
+      var sprite = node.addComponent(cc.Sprite);
+      sprite.spriteFrame = spriteFrame;
+      node.height = 150;
+      node.width = 150;
+      node.parent = imgnode;
+    });
+    // cc.log("3" + JSON.stringify(window.thingsInListArray));
+
+    this.buybtnsensor.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        let action1 = cc.scaleTo(0.1, 1.5);
+        let action2 = cc.scaleTo(0.05, 1);
+        this.buybtn.runAction(cc.sequence(action1, action2));
+
+        if (window.theMoneyOfPlayer >= Number(item.price)) {
+          window.theMoneyOfPlayer -= Number(item.price);
+          cc.log(window.theJSONOfAllItems);
+          window.theJSONOfAllItems.map(obj => {
+            if (obj.itemName == item.itemName) {
+              obj.quantity += 1;
+              this.hasOwned.string = "已拥有：" + String(obj.quantity);
+            }
+          });
+        }
+        cc.sys.localStorage.setItem(
+          "itemList",
+          JSON.stringify(window.theJSONOfAllItems)
+        );
+        cc.sys.localStorage.setItem(
+          "theMoneyOfPlayer",
+          window.theMoneyOfPlayer
+        );
+
+        // cc.log("buybtn");
+        // cc.log(window.theMoneyOfPlayer);
+      },
+      this
+    );
   }
-
-  // start() {}
-
-  // update (dt) {},
 });
